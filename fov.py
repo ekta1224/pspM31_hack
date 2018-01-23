7dffdd90e63c64c02e84c1aeb5024958826063e3
Ekta Patel <ektapatel@email.arizona.edu>
2018-01-23 15:52:43 -0700

    made table of all M33 calculations

--- fov.py
+++ fov.py
@@ -22,22 +22,36 @@ def R_fov(fov, distance, sq=False):
 m33r = 960. 
 rvir = 120.
 Z = 1.5
-nlum = 7.4 
+nlum = 7.4
 fov = 2.2
 
 print 'K(R):', K(R_fov(fov, m33r )/rvir, Z)
 #print N_fov(nlum, R_fov(fov, m33r)/rvir, Z) #satellites per pointing
 
+fovs = [1., 1.5, 2.2]
 nlums = [10.5, 7.4, 3.7, 1.3]
 rs = [794., 880., 960.]
 for nlum in nlums:
     print N_fov(nlum, R_fov(fov, m33r)/rvir, Z) #satellites per pointing
 
 #below the R_fov of hypersuprimecam (R~0.06), K pretty much goes to negative numbers
+rvir = 120.
+Z = 1.5
+nlum = 10.5
+fov = 2.2
+
 for m33r in rs:
     print m33r
-    print 'R_fov', R_fov(1., m33r), R_fov(1.5, m33r), R_fov(2.2, m33r)
-    minfov = 120*0.06*2.*180/np.pi/m33r
-    print 'minimum FOV (deg) as fn of m33r', minfov
+    for fov in fovs:
+        print fov
+        rfov = R_fov(fov, m33r)
+        k = K(R_fov(fov, m33r)/rvir, Z)
+        nfov = N_fov(nlum, rfov/rvir,Z)
+        points = rvir**2./R_fov(fov, m33r)**2.
+        print ' K, R_fov,  Npoint %s & %s & %s & %s & %s & %s & %s'%(round(k, 3), round(rfov,2), int(round(points,0)), round(N_fov(10.5, rfov/rvir, Z),3), round(N_fov(7.4, rfov/rvir, Z),3), round(N_fov(3.7, rfov/rvir, Z),3), round(N_fov(1.3, rfov/rvir, Z),3))
+        minfov = 120*0.06*2.*180/np.pi/m33r
+        #print 'minimum FOV (deg) as fn of m33r', minfov
     
 print N_fov(10.5, R_fov(0.85, 960.)/rvir, Z) #satellites per pointing
+
+
